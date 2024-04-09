582aa94bb14447674867b17e74ebfc8e2eb68367
Felix Guerin <guerinfelix08@gmail.com>
2024-04-09 15:17:51 -0400

    I18n doc (#2)
    * Refactor hooks
    
    * Add docs and fix export
    
    * Bump version
    
    * Update readme and types
    
    * Fix types
    
    * Bump version

--- packages/i18n/src/types/index.d.ts
+++ packages/i18n/src/types/index.d.ts
@@ -1,9 +1,19 @@
+import type { createTranslate } from '../translations/client/createTranslate';
+
 export type Locale = `${string}-${string}`;
 export type Language = string;
 export type Region = string;
 
-export type LangInfo = {
-	locale: Locale;
-	language: string;
-	region: string;
+/**
+ * The properties added in the event locals by the i18n handle.
+ */
+export type LangInfo<Locales extends readonly Locale[] = string[]> = {
+	locale: Locales[number];
+	language: Locales[number] extends `${infer Lang}-${infer _}` ? Lang : string;
+	region: Locales[number] extends `${_}-${infer Region}` ? Region : string;
 };
+
+/**
+ * The translate function returned from `createTranslate()`
+ */
+export type TranslateFunction<T extends AnonymousObject> = ReturnType<typeof createTranslate<T>>;
