f0779fefae96ffca10e217f2425963a1612e9c8a
Felix Guerin <guerinfelix08@gmail.com>
2024-04-09 15:29:25 -0400

    I18n tests (#3)
    * Add tests
    
    * Use pnpm

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,19 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 
-export default function config() {
-	return defineConfig({
-		build: {
-			lib: {
-				// Could also be a dictionary or array of multiple entry points
-				entry: 'src/index.ts',
-				name: 'index',
-				// the proper extensions will be added
-				fileName: 'index'
-			},
-			rollupOptions: {
-				// make sure to externalize deps that shouldn't be bundled
-				// into your library
-				external: ['svelte'],
-				output: {
-					// Provide global variables to use in the UMD build
-					// for externalized deps
-					globals: {
-						vue: 'svelte'
-					}
-				}
-			}
-		}
-	});
-}
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		css: true,
+		// We need to disable parallelism in CI because we can not afford to have
+		// multiple process/browsers running at the same time.
+		threads: !process.env.CI,
+		include: ['./packages/**/*.spec.{js,mjs,cjs,ts}'],
+		coverage: {
+			exclude: ['setup.ts', 'test/mocks/**', 'vite/plugins/**']
+		},
+		// This alias is needed until this is resolved: https://github.com/vitest-dev/vitest/issues/2834
+		alias: [{ find: /^svelte$/, replacement: 'svelte/internal' }]
+	}
+});
