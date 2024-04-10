63f583d9a1e2e04950bc2c74b91acfc52a31a11d
f-elix <guerinfelix08@gmail.com>
2024-04-10 17:09:31 -0400

    Throw if config is not defined

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -6,4 +6,11 @@ export const defineConfig = (c: Config) => {
 	config = c;
 };
 
-export const getConfig = () => config;
+export const getConfig = () => {
+	if (!config) {
+		throw new Error(
+			'288 Toolkit config is not defined. Use `defineConfig` inside `src/hooks.ts` to set it.'
+		);
+	}
+	return config;
+};
