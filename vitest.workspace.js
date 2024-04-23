2000a4867b8dde23cd91fa57a56a5da2f442d7ca
f-elix <guerinfelix08@gmail.com>
2024-04-23 10:52:24 -0400

    Add vitest workspace

---
+++ vitest.workspace.js
@@ -0,0 +1,8 @@
+import { defineWorkspace } from 'vitest/config';
+
+export default defineWorkspace([
+	'./packages/i18n/src/vite.config.ts',
+	'./packages/format/vite.config.ts',
+	'./packages/http/vite.config.ts',
+	'./packages/dates/vite.config.ts'
+]);
