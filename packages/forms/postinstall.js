1640a760a7ea7427c8c96a3a08583704a061adb2
f-elix <guerinfelix08@gmail.com>
2024-05-14 14:33:11 -0400

    Dont run postinstall script in toolkit

--- packages/forms/postinstall.js
+++ packages/forms/postinstall.js
@@ -1,4 +1,26 @@
 import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Skip this script if we are in a CI environment
+if (process.env.CI) {
+	process.exit(0);
+}
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const gitConfig = await fs.readFile(path.join(__dirname, '../../.git/config'), {
+	encoding: 'utf-8'
+});
+
+const inToolkit = !!gitConfig?.match(
+	new RegExp('\\[remote "origin"\\]\\s+url = .+288-toolkit\\.git')
+)?.[0];
+
+// Skip this script if we are in the toolkit
+if (inToolkit) {
+	process.exit(0);
+}
 
 const constantsPath = './dist/constants.js';
 const constants = await fs.readFile(constantsPath, { encoding: 'utf-8' });
