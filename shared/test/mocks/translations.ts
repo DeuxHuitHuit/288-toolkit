a57d64cb7f5851d37a10b008efa0d72ce8168f76
Nicolas Brassard <nitriques@users.noreply.github.com>
2024-10-29 13:16:43 -0400

    (fix) Make the test suite pass
    1. Ditch the vite config: we only need vitest
    2. Do not load sveltekit plugin in vitesse (we can if needed in the
       future)
    3. Add missing translation mocks
    4. Use js files for vitest config

---
+++ shared/test/mocks/translations.ts
@@ -0,0 +1,3 @@
+export const t = (path: string) => {
+	return path;
+};
--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,9 +1,9 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-	'./packages/i18n/src/vite.config.ts',
-	'./packages/format/vite.config.ts',
-	'./packages/http/vite.config.ts',
-	'./packages/dates/vite.config.ts',
-	'./packages/url/vite.config.ts'
+	'./packages/i18n/vitest.config.js',
+	'./packages/format/vitest.config.js',
+	'./packages/http/vitest.config.js',
+	'./packages/dates/vitest.config.js',
+	'./packages/url/vitest.config.js'
 ]);
