4603984c38b43590cd7ef512b076eac1c1441b46
Nicolas Brassard <nitriques@users.noreply.github.com>
2024-10-29 13:16:43 -0400

    (fix) Make the test suite pass
    1. Ditch the vite config: we only need vitest
    2. Do not load sveltekit plugin in vitesse (we can if needed in the
       future)
    3. Add missing translation mocks
    4. Use js files for vitest config

--- packages/format/test/setup.ts
+++ packages/format/test/setup.ts
@@ -1,1 +1,13 @@
+import { vi } from 'vitest';
 import '../../../shared/test/mocks/sveltekit';
+
+// Mock translation module
+vi.mock('../src/lib/translations/filesize/index.js', async () => {
+	return import(__dirname + '/../../../shared/test/mocks/translations.ts');
+});
+vi.mock('../src/lib/translations/relativeTime/index.js', async () => {
+	return import(__dirname + '/../../../shared/test/mocks/translations.ts');
+});
+vi.mock('../src/lib/translations/duration/index.js', async () => {
+	return import(__dirname + '/../../../shared/test/mocks/translations.ts');
+});
---
+++ vitest.shared.js
@@ -0,0 +1,10 @@
+import { defineProject } from 'vitest/config';
+
+export default defineProject({
+	test: {
+		include: ['./test/**/*.spec.ts'],
+		environment: 'jsdom',
+		css: true,
+		globals: true
+	}
+});
