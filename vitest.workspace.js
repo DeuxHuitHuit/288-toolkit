256c959d75fe09b18b927c614db855b576567087
Nicolas Brassard <nitriques@users.noreply.github.com>
2024-10-29 14:43:10 -0400

    (chore) Add more missing tests

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,6 +1,7 @@
 import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
+	'./packages/arrays/vitest.config.js',
 	'./packages/i18n/vitest.config.js',
 	'./packages/format/vitest.config.js',
 	'./packages/http/vitest.config.js',
