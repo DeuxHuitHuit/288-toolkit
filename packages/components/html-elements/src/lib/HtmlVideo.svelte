c1aa4ebc81e28a649cbb36b14e25024b144dd98e
f-elix <guerinfelix08@gmail.com>
2024-10-30 10:28:58 -0400

    Properly handle muted and controls video attributes

--- packages/components/html-elements/src/lib/HtmlVideo.svelte
+++ packages/components/html-elements/src/lib/HtmlVideo.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
 	export let autoplay = false;
 	export let disableremoteplayback = true;
+	export let muted = false;
+	export let controls = true;
+	export let el: HTMLVideoElement | undefined = undefined;
+
+	const updateAttribute = (attr: string, value: boolean) => {
+		if (el) {
+			el[attr] = value;
+		}
+	};
+
+	$: if (el) {
+		updateAttribute('muted', muted);
+		updateAttribute('controls', controls);
+	}
 </script>
 
 <!-- svelte-ignore a11y-media-has-caption -->
 <video
+	autoplay={autoplay ? true : null}
 	playsinline={autoplay || null}
-	{autoplay}
 	{disableremoteplayback}
 	x-webkit-airplay={disableremoteplayback ? 'deny' : 'allow'}
+	bind:this={el}
 	{...$$restProps}
 >
 	<slot />
