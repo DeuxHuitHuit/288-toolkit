0c93aa6e17fd879aa89f5d31b310834d4a8d94db
Felix Guerin <guerinfelix08@gmail.com>
2024-11-28 11:29:16 -0500

    Svelte 5 - html-elements (#23)

--- packages/components/html-elements/src/lib/HtmlImg.svelte
+++ packages/components/html-elements/src/lib/HtmlImg.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import type { Maybe } from '@288-toolkit/types';
 
-	export let src: Maybe<string>;
-	export let srcset: Maybe<string> = null;
-	export let alt: Maybe<string> = null;
+	interface Props {
+		src: Maybe<string>;
+		srcset?: Maybe<string>;
+		alt?: Maybe<string>;
+		[key: string]: any;
+	}
+
+	let { src, srcset = null, alt = null, ...rest }: Props = $props();
 </script>
 
-<img srcset={srcset || null} src={!srcset ? src : null} alt={alt || ''} {...$$restProps} />
+<img srcset={srcset || null} src={!srcset ? src : null} alt={alt || ''} {...rest} />
