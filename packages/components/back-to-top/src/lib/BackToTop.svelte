71261dfe35ec5cbb758924d297b2c73cd89fc310
f-elix <guerinfelix08@gmail.com>
2025-01-09 14:36:57 -0500

    Update props

--- packages/components/back-to-top/src/lib/BackToTop.svelte
+++ packages/components/back-to-top/src/lib/BackToTop.svelte
@@ -5,17 +5,17 @@ A button that scrolls the window back to the top when clicked.
 	import { motionSafeScrollBehavior } from '@288-toolkit/ui';
 
 	interface Props {
-		class?: string;
 		children?: import('svelte').Snippet;
+		[key: string]: unknown;
 	}
 
-	let { class: classes = '', children }: Props = $props();
+	let { children, ...rest }: Props = $props();
 
 	const onClick = () => {
 		window.scrollTo({ top: 0, left: 0, behavior: motionSafeScrollBehavior() });
 	};
 </script>
 
-<button type="button" tabindex="-1" aria-hidden="true" class={classes} onclick={onClick}>
+<button type="button" tabindex="-1" aria-hidden="true" onclick={onClick} {...rest}>
 	{@render children?.()}
 </button>
