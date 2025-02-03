ede957fcbbc05724bf8b40ed63f255d585912483
Nicolas Brassard <nitriques@users.noreply.github.com>
2025-02-03 15:06:33 -0500

    (fix) Add missing can parse in barrel

--- packages/url/src/index.ts
+++ packages/url/src/index.ts
@@ -2,3 +2,4 @@ export * from './createEntryUrlBuilder.js';
 export * from './isExternalUrl.js';
 export * from './validateSameOrigin.js';
 export * from './parsedUrl.js';
+export * from './urlCanParse.js';
