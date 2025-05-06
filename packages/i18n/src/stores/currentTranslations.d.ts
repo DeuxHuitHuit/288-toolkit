941aa98d0f28a2377575e510fd04f3c7d8315045
f-elix <guerinfelix08@gmail.com>
2025-05-06 11:10:54 -0400

    i18n

--- packages/i18n/src/stores/currentTranslations.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="svelte" />
-import type { AnonymousObject } from '@288-toolkit/types';
-export declare const currentTranslations: import("svelte/store").Writable<Record<string, AnonymousObject>>;
--- packages/i18n/src/stores/currentTranslations.ts
+++
@@ -1,4 +0,0 @@
-import type { AnonymousObject, Maybe } from '@288-toolkit/types';
-import { writable } from 'svelte/store';
-
-export const currentTranslations = writable<Record<string, AnonymousObject>>({});
