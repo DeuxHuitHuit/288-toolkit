8696354933d8d9dadba994d9dabc8eec8f1ffd85
patrickM288 <92889616+patrickM288@users.noreply.github.com>
2026-04-16 16:50:30 +0000

    (chore) Format code

--- packages/ui/src/scrollToAnchor.ts
+++ packages/ui/src/scrollToAnchor.ts
@@ -5,7 +5,7 @@ import { motionSafeScrollBehavior } from './motionSafeScrollBehavior.js';
  * This action will smoothly scroll to the anchor link's target if it exists while
  * preserving the browser's default behavior.
  * @param node The anchor link
- * 
+ *
  * @deprecated Use native anchor behavior with css property "scroll-behavior: smooth;" on html instead.
  */
 export const scrollToAnchor = (node: HTMLAnchorElement) => {
