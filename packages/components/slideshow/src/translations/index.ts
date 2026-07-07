d84454cbe6e97bd9572b60c5ba4b0473c8714849
Nicolas Brassard <nitriques@users.noreply.github.com>
2026-07-07 18:39:59 -0400

    (fix) Broken type export

--- packages/components/slideshow/src/translations/index.ts
+++ packages/components/slideshow/src/translations/index.ts
@@ -1,6 +1,6 @@
-import { createTranslate } from '@288-toolkit/i18n';
+import { createTranslate, type TranslateFunction } from '@288-toolkit/i18n';
 import type { SlideshowTranslations } from './en.js';
 
 export const key = 'slideshow';
 
-export const t = createTranslate<SlideshowTranslations>(key);
+export const t: TranslateFunction<SlideshowTranslations> = createTranslate<SlideshowTranslations>(key);
